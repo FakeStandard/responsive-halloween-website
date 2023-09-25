270294940d3bece2786fa8116e5ac041d03db962
riley.lin <rileylin.space@gmail.com>
2023-09-25 16:05:55 +0800

    add header

---
+++ assets/js/style.js
@@ -0,0 +1,26 @@
+// SHOW MENU
+const navMenu = document.getElementById("nav-menu")
+const navToggle = document.getElementById("nav-toggle")
+const navClose = document.getElementById("nav-close")
+
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+// REMOVE MENU MOBILE
+const navLink = document.querySelectorAll('.nav-link')
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(f => f.addEventListener('click', linkAction))
