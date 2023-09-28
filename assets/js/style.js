8ad900be41c65a203c5ed9e20fad3639f2815bd9
riley.lin <rileylin.space@gmail.com>
2023-09-28 23:11:36 +0800

    add category

--- assets/js/style.js
+++ assets/js/style.js
@@ -35,3 +35,13 @@ let homeSwiper = new Swiper(".home-swiper", {
         clickable: true
     },
 });
+
+// CHANGE BACKGROUND HEADER
+function scrollHeader() {
+    const header = document.getElementById('header')
+
+    if (this.scrollY >= 50) header.classList.add('scroll-header')
+    else header.classList.remove('scroll-header')
+}
+
+window.addEventListener('scroll', scrollHeader)
