633f94a91337daae301031d285833a9fe302a06a
riley.lin <rileylin.space@gmail.com>
2023-09-29 10:46:32 +0800

    add scroll section

--- assets/js/style.js
+++ assets/js/style.js
@@ -53,3 +53,25 @@ let newSwiper = new Swiper(".new-swiper", {
     spaceBetween: 16,
     loop: 'true',
 });
+
+// SCROLL SECTIONS ACTIVE LINK
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50
+
+        sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+        } else {
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollActive)
